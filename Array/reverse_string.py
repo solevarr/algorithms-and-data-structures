5976384a786a2139307b673b7e394666ae6408b3
Adika <adilegvhs@mail.ru>
2020-04-30 11:02:23 +0000

    added simple string reverse

---
+++ Array/reverse_string.py
@@ -0,0 +1,26 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+def reverse(some_string):
+  if not isinstance(some_string, str):
+    return 'hmm that is not good'
+  string_list = []
+  for char in some_string:
+    string_list.append(char)  
+  string_list = string_list[::-1]
+  reversed_string = ''.join(string_list)
+  logging.info(reversed_string)
+  return reversed_string
+
+def reverse_another(some_string):
+  if not isinstance(some_string, str):
+    return 'hmm that is not good'
+  reversed_string=  ''.join(reversed(list(some_string)))
+  logging.info(reversed_string)
+  return reversed_string
+  
+
+if __name__ == '__main__':
+  some_string = 'Hoho haha'
+  reverse_another(some_string)
